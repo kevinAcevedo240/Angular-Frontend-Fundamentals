c2c4e2b0b8368a095230cf5b92f359d9f830b24d
Kevin Julian Acevedo Salazar <kjacevedos@outsourcing.com.co>
2023-06-05 15:32:15 -0500

    Angular Concepts p1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,75 @@ export class AppComponent {
    name = 'Kevin';
   age = 21;
   img = 'https://definicion.de/wp-content/uploads/2013/03/perro-1.jpg';
+  btnDisabled = true;
+  InputDisabled = false;
+  person ={
+    name: 'Kevin Acevedo',
+    age: 24,
+    avatar:'https://definicion.de/wp-content/uploads/2013/03/perro-1.jpg'
+  }
+  names: string[] = ['Kevin','Julian','Pedro','Pablo'];
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/Images/Toy.jpeg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/Images/Bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/Images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/Images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/Images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/Images/glasses.jpg'
+    }
+  ]
+
+  toogleButton(){
+    this.InputDisabled = !this.InputDisabled;
+  }
+
+  increaseAge(){
+    this.person.age++;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number){
+    this.names.splice(index, 1);
+  }
+
 }
