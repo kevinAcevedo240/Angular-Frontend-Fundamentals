019d0bfc61c7961866a28f523bf0d2504ef76dcc
kevinAcevedo240 <keju.acevedosa24@gmail.com>
2023-06-05 02:28:51 -0500

    Primeros Cambios

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+   name = 'Kevin';
+  age = 21;
+  img = 'https://definicion.de/wp-content/uploads/2013/03/perro-1.jpg';
+}
